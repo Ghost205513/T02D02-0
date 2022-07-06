12399e50050b46529dd2fa25c2c426eab86e6009
Lucio Anastasia <lucioana@mi-j1.msk.21-school.ru>
2022-07-06 20:35:25 +0300

    quest 2

---
+++ src/edit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 'Enter path to file:'
+read path
+if [ -f "$path" ] && [[ $path = /* ]]
+then
+    echo 'Enter string to replace:'
+    read old_str
+    if grep -q "$old_str" $path
+    then
+        echo 'Enter replaced string:'
+        read new_str
+        touch temp
+        sed -i '' 's/'$old_str'/'$new_str'/' $path
+        #size = $(ls -l $path | tr -s ' ' | cut -d ' ' -f 5)
+        #date_of_last_modificate = $(date -r $path +'%F %R')
+        #hash = $(shasum -a 256 path | cut -d ' ' -f 1)
+        echo "$path - $(ls -l $path | tr -s ' ' | cut -d ' ' -f 5) - $(date -r $path +'%F %R') - $(shasum -a 256 $path | cut -d ' ' -f 1) - sha256" >> files.log
+    else
+        echo 'No such string in the file'
+    fi
+else
+    echo 'Wrong path to file'
+fi
